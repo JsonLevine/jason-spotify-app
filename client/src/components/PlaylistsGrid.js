5d651500a954f57452c8ce578f2c2baadb8a00c6
JsonLevine <50605966+JsonLevine@users.noreply.github.com>
2024-04-24 09:41:05 -0400

    Updates

--- client/src/components/PlaylistsGrid.js
+++ client/src/components/PlaylistsGrid.js
@@ -8,7 +8,7 @@ const PlaylistsGrid = ({ playlists }) => (
         {playlists.map((playlist, i) => (
           <li className="grid__item" key={i}>
             <Link className="grid__item__inner" to={`/playlists/${playlist.id}`}>
-              {playlist.images.length && playlist.images[0] && (
+              {playlist.images?.length && playlist.images[0] && (
                 <div className="grid__item__img">
                   <img src={playlist.images[0].url} alt={playlist.name} />
                 </div>
